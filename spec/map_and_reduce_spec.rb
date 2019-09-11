cdbdb294efdf4cb0120bbb7349f56e5dc604c8ed
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-11 21:43:27 +0000

    Automatically backed up by Learn

--- spec/map_and_reduce_spec.rb
+++ spec/map_and_reduce_spec.rb
@@ -64,10 +64,10 @@ describe 'my own reduce-like methods' do
     end
   end
 
-  describe "reduce_to_any_true returns false when no truthy value is present" do
+  describe "reduce_to_any_true1 returns false when no truthy value is present" do
     it "reduces correctly" do
       source_array = [ false, nil, nil, nil]
-      expect(reduce_to_any_true(source_array)).to eq(false)
+      expect(reduce_to_any_true1(source_array)).to eq(false)
     end
   end
 end
