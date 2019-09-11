9b7f2b887e3fa2a68a51bb7d3a1be78cbec9fcde
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-11 21:09:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,40 @@
-# My Code here....
+def map_to_negativize(arr)
+    index = 0 
+    while index < arr.length do
+        arr[index] = arr[index]*(-1)
+        index+=1
+    end
+    return arr
+end
+
+def map_to_no_change (arr)
+  index = 0 
+  if (arr)
+    arr
+  end
+  return arr
+end
+
+def map_to_double(arr)
+  index = 0 
+  while index < arr.length do
+    arr[index] = arr[index]*2
+    index+=1 
+  end
+  return arr
+end
+
+def map_to_square(arr)
+  index = 0 
+  while index < arr.length do
+    arr[index] = arr[index] ** 2 
+    index+=1 
+  end
+  return arr
+end
+
+def reduce_to_total(source_arr,starting_point)
+index = 0 
+while index < source_arr.length do
+  starting_point
+  
